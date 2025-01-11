215deb85c6646ace7f39bbb27d3f57a75208a9e6
jashvanth370 <jashva25@gmail.com>
2025-01-11 14:10:52 +0530

    initialation commit for the project

---
+++ Weather/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+/* images */
+import ClearIcon from "/assets/clear.png";
+import CloudIcon from "/assets/cloud.png";
+import DrizzleIcon from "/assets/drizzle.png";
+import RainIcon from "/assets/rain.png";
+import SnowIcon from "/assets/snow.png";
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        test
+      
+      </div>
+    </>
+  )
+}
+
+export default App
