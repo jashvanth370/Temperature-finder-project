683c3388addb7a2dd74af88b4f0efa920fe11bc9
jashvanth370 <jashva25@gmail.com>
2025-01-11 17:46:22 +0530

    front end finish

--- Weather/src/App.jsx
+++ Weather/src/App.jsx
@@ -7,9 +7,10 @@ import CloudIcon from "./assets/cloud01.png";
 import DrizzleIcon from "./assets/drizzle01.png";
 import RainIcon from "./assets/rain01.png";
 import SnowIcon from "./assets/snow01.png";
-import WindIcon from "./assets/wind01.png"
+import WindIcon from "./assets/wind01.png";
+import HumidityIcon from "./assets/humidity01.png";
 
-const WeatherDetails=({icon,temp,city,lat,log})=>{
+const WeatherDetails=({icon,temp,city,lat,log,humidity,wind})=>{
   return(
     <>
   <div className='image'>
@@ -30,6 +31,22 @@ const WeatherDetails=({icon,temp,city,lat,log})=>{
       <span>{log}</span>
     </div>
   </div>
+  <div className="data-container">
+    <div className="element">
+      <img src={HumidityIcon} alt="Humidity" className='icon'/>
+      <div className="data">
+        <div className="humidity-percent">{humidity}%</div>
+        <div className="text">Humidity</div>
+      </div>
+    </div>
+    <div className="element">
+      <img src={WindIcon} alt="Wind" className='icon'/>
+      <div className="data">
+        <div className="wind-percent">{wind} km/h</div>
+        <div className="text">Wind Speed</div>
+      </div>
+    </div>
+  </div>
   </>
   );
 }
@@ -39,6 +56,9 @@ function App() {
   const [city,setCity] = useState("Batticaloa");
   const [lat,setLat] =useState(0);
   const [log,setLong] =useState(0);
+  const [humidity,setHumidity] =useState(0);
+  const [wind,setWind] =useState(0);
+
   return (
     <>
       <div className='container'>
@@ -50,8 +70,7 @@ function App() {
             <img src={SearchIcon} alt="search" width="25px"/>
           </div>
         </div>        
-      <WeatherDetails icon={icon} temp={temp} city={city} lat={lat} log={log}
-      />
+      <WeatherDetails icon={icon} temp={temp} city={city} lat={lat} log={log} humidity={humidity} wind={wind}/>
       </div>
     </>
   )
