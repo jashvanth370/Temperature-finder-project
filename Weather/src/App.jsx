df53e38393b9bd39fb5e4c073fb3eee67e4e9ef0
jashvanth370 <jashva25@gmail.com>
2025-01-11 16:55:34 +0530

    front end design

--- Weather/src/App.jsx
+++ Weather/src/App.jsx
@@ -9,18 +9,36 @@ import RainIcon from "./assets/rain01.png";
 import SnowIcon from "./assets/snow01.png";
 import WindIcon from "./assets/wind01.png"
 
-const WeatherDetails=({icon})=>{
+const WeatherDetails=({icon,temp,city,lat,log})=>{
   return(
     <>
   <div className='image'>
     <img src={icon} alt="image" />
+  </div>
+  <div className='temp'>
+    {temp} °C
+  </div>
+  <div className='location'> {city}</div>
+  <div className="cord">
+    <div>
+      <span className="lat">latitude</span>
+      <span>{lat}</span>
+    </div>
+
+    <div>
+      <span className="log">longitude</span>
+      <span>{log}</span>
+    </div>
   </div>
   </>
   );
 }
 function App() {
   const [icon,setIcon] = useState(RainIcon);
-
+  const [temp,setTemp] = useState(0);
+  const [city,setCity] = useState("Batticaloa");
+  const [lat,setLat] =useState(0);
+  const [log,setLong] =useState(0);
   return (
     <>
       <div className='container'>
@@ -32,7 +50,8 @@ function App() {
             <img src={SearchIcon} alt="search" width="25px"/>
           </div>
         </div>        
-      <WeatherDetails icon={icon}/>
+      <WeatherDetails icon={icon} temp={temp} city={city} lat={lat} log={log}
+      />
       </div>
     </>
   )
