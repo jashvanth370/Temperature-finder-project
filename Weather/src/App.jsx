d948a5573d43cccfe399bbd01f4f8cbca4800e94
jashvanth370 <jashva25@gmail.com>
2025-01-11 15:10:27 +0530

    Styles add for the front page of the project

--- Weather/src/App.jsx
+++ Weather/src/App.jsx
@@ -1,21 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 
-/* images */
-import ClearIcon from "/assets/clear.png";
-import CloudIcon from "/assets/cloud.png";
-import DrizzleIcon from "/assets/drizzle.png";
-import RainIcon from "/assets/rain.png";
-import SnowIcon from "/assets/snow.png";
+import SearchIcon from "./assets/search.png"
+import ClearIcon from "./assets/clear01.png";
+import CloudIcon from "./assets/cloud01.png";
+import DrizzleIcon from "./assets/drizzle01.png";
+import RainIcon from "./assets/rain01.png";
+import SnowIcon from "./assets/snow01.png";
+import WindIcon from "./assets/wind01.png"
 
+const WeatherDetails=({icon})=>{
+  return(
+    <>
+  <div className='image'>
+    <img src={icon} alt="image" />
+  </div>
+  </>
+  );
+}
 function App() {
-  
+  const [icon,setIcon] = useState(RainIcon);
 
   return (
     <>
-      <div>
-        test
-      
+      <div className='container'>
+        <div className='input-container'>
+          <input type="text" 
+          className='cityInput'
+          placeholder='Search City'/>
+          <div className='search-icon'>
+            <img src={SearchIcon} alt="search" width="25px"/>
+          </div>
+        </div>        
+      <WeatherDetails icon={icon}/>
       </div>
     </>
   )
