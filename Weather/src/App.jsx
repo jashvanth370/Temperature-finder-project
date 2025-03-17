1832535fed79d8a926cf7fa66cbaf8757e3caa87
jashvanth370 <jashva25@gmail.com>
2025-03-17 11:49:06 +0530

    complete the weather app project

--- Weather/src/App.jsx
+++ Weather/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-import SearchIcon from "./assets/search.png"
-import ClearIcon from "./assets/clear01.png";
-import CloudIcon from "./assets/cloud01.png";
-import DrizzleIcon from "./assets/drizzle01.png";
-import RainIcon from "./assets/rain01.png";
-import SnowIcon from "./assets/snow01.png";
-import WindIcon from "./assets/wind01.png";
-import HumidityIcon from "./assets/humidity01.png";
+import searchIcon from "./assets/search.png"
+import clearIcon from "./assets/clear01.png";
+import cloudIcon from "./assets/cloud01.png";
+import drizzleIcon from "./assets/drizzle01.png";
+import rainIcon from "./assets/rain01.png";
+import snowIcon from "./assets/snow01.png";
+import windIcon from "./assets/wind01.png";
+import humidityIcon from "./assets/humidity01.png";
 
 const WeatherDetails=({icon,temp,city,lat,log,humidity,wind})=>{
   return(
@@ -33,14 +33,14 @@ const WeatherDetails=({icon,temp,city,lat,log,humidity,wind})=>{
   </div>
   <div className="data-container">
     <div className="element">
-      <img src={HumidityIcon} alt="Humidity" className='icon'/>
+      <img src={humidityIcon} alt="Humidity" className='icon'/>
       <div className="data">
         <div className="humidity-percent">{humidity}%</div>
         <div className="text">Humidity</div>
       </div>
     </div>
     <div className="element">
-      <img src={WindIcon} alt="Wind" className='icon'/>
+      <img src={windIcon} alt="Wind" className='icon'/>
       <div className="data">
         <div className="wind-percent">{wind} km/h</div>
         <div className="text">Wind Speed</div>
@@ -54,7 +54,7 @@ function App() {
   
 
   let api_key ="a310db4cb8a196104c769b6f86c3e3ff";
-  const [icon,setIcon] = useState(RainIcon);
+  const [icon,setIcon] = useState(rainIcon);
   const [temp,setTemp] = useState(0);
   const [city,setCity] = useState("Chennai");
   const [lat,setLat] =useState(0);
@@ -64,6 +64,24 @@ function App() {
   const [text,setText] =useState("chennai");
   const [cityNotFound, setCityNotFound] =useState(false);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const weatherIconMap = {
+    "01d":clearIcon,
+    "01n":clearIcon,
+    "02d":cloudIcon,
+    "02n":cloudIcon,
+    "03n":drizzleIcon,
+    "03d":drizzleIcon,
+    "04d":drizzleIcon,
+    "04n":drizzleIcon,
+    "09d":rainIcon,
+    "09n":rainIcon,
+    "10d":rainIcon,
+    "10n":rainIcon,
+    "13n":snowIcon,
+    "13d":snowIcon,
+  }
 
   const search=async()=>{
     setLoading(true);
@@ -72,7 +90,7 @@ function App() {
     try{
       let res =await fetch(url);
       let data =await res.json();
-      //console.log(data);
+      console.log(data);
       if(data.cod==="404"){
         console.error("City Not Found");
         setCityNotFound(true);
@@ -85,9 +103,13 @@ function App() {
       setCity(data.name);
       setLat(data.coord.lat);
       setLong(data.coord.lon);
+      const weatherIconMap = data.weather[0].icon;
+      setIcon(weatherIconMap[weatherIconMap] || clearIcon);
+      setCityNotFound(false);
     }
     catch(error){
       console.error("An error occured:", error.message);
+      setError("An error occure while fetching data.");
     }
     finally{
       setLoading(false);
@@ -104,21 +126,37 @@ function App() {
     }
   };
 
+  useEffect(function () {
+    search();
+  }, []);
+
   return (
     <>
       <div className='container'>
         <div className='input-container'>
           <input type="text" 
           className='cityInput'
-          placeholder='Search City'onChange={handleCity}
+          placeholder='Search City'
+          onChange={handleCity}
           value={text}
           onKeyDown={handleKeyDown}/>
+          
           <div className='search-icon'>
-            <img src={SearchIcon} alt="search" width="25px" onClick={()=> search()}/>
+            <img src={searchIcon} 
+            alt="search" 
+            width="25px" 
+            onClick={()=> search()}/>
           </div>
-        </div>        
-      <WeatherDetails icon={icon} temp={temp} city={city} lat={lat} log={log} humidity={humidity} wind={wind}/>
+        </div>    
+        {loading && <div className="loading-message">Loading...</div>}
+        {error && <div className="error-message">{error}</div>}
+        {cityNotFound && <div className="city-not-message">City Not Found </div>    }
+      
+      {!loading && !cityNotFound && <WeatherDetails icon={icon} temp={temp} city={city} lat={lat} log={log} humidity={humidity} wind={wind}/>}
       </div>
+
+      
+
     </>
   )
 }
